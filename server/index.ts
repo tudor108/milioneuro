1af9a8dc10b1592ca05b594d35677c2bfc61bccb
Numele tău <emailul@tu>
2025-11-02 19:20:25 +0200

    dwwdwwsws

--- server/index.ts
+++ server/index.ts
@@ -72,10 +72,12 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
+  const shouldReusePort = process.platform !== "win32";
+
   server.listen({
     port,
     host: "0.0.0.0",
-    reusePort: true,
+    ...(shouldReusePort ? { reusePort: true } : {}),
   }, () => {
     log(`serving on port ${port}`);
   });
